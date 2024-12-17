0a424f4908fa7631f20e96e08f1cfc8441be6014
Demomech <123835253+Demomech@users.noreply.github.com>
2024-12-17 23:45:16 +0100

    Add files via upload

---
+++ game/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int health;
+    health=100;
+
+    printf("Welcome to the Kingdom of Larion!\n");
+    printf("You have %d health points.\n", health);
+
+    return 0;
+}
